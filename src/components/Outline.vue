412986cf7a819e697fb6f0dd37225c8e275133b4
Jerry Balderas <balderasfam@gmail.com>
2023-01-30 09:49:41 -0600

    Added outline

---
+++ src/components/Outline.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="hello">
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+<li>Prologue</li>
+<li>John the Baptist's Birth</li>
+<li>Angel visits Mary </li>
+<li>Mary visits Elizabeth</li>
+<li>The birth of Jesus, the Shepherds</li>
+<li>Presentation of Jesus to the temple</li>
+<li>The wise men visit Jesus </li>
+<li>Journey to Egypt while Herod slays children</li>
+<li>Death of Herod - return to Egypt</li>
+<li>Jesus at 12 years of age</li>
+<li>John the Baptist begins ministry</li>
+<li>The baptism of Jesus</li>
+<li>The geneology of Jesus </li>
+<li>The temptation of Jesus</li>
+<li>Jesus' first miracle</li>
+<li>First passover of Jesus' ministry</li>
+<li>Jesus cleanses the temple</li>
+<li>Nicodemus visits at night</li>
+<li>Jesus baptizes, through his disciples.  John continues support </li>
+<li>Jesus converses with woman in Samaria</li>
+<li>Jesus arrives in Galilee</li>
+<li>Jesus rejection at Nazareth</li>
+<li>Jesus in Capernaum</li>
+<li>Jesus heals Peter's mother-in-law</li>
+<li>Jesus departs from Capernaum</li>
+<li>A preaching journey in Galilee</li>
+<li>Miraculous catch of fish - first disciples called </li>
+<li>Healing the leper</li>
+<li>Healing of the paralytic</li>
+<li>THe call of Levi</li>
+<li>Question about fasting</li>
+<li>Jesus heals man at Bethesda</li>
+<li>Two blind men healed</li>
+<li>Plucking grain from the sabbath</li>
+<li>Healing of man with withered hand</li>
+<li>Call of the twelve</li>
+<li>Jesus heals the multitudes </li>
+<li>Sermon on the Mount</li>
+<li>End of Sermon</li>
+<li>Healing the centurion's servant</li>
+<li>Healing widow's son at Naim</li>
+<li>John's imprisonment</li>
+<li>John's question to Jesus</li>
+<li>Jesus' words about John</li>
+<li>Woman with the ointment</li>
+<li>Parables of Jesus</li>
+<li>Jesus' true relatives</li>
+<li>Stilling of the storm</li>
+<li>The Gadarene demoniac</li>
+<li>Jairus' daughter and a woman's faith</li>
+<li>Jesus rejected at Nazareth</li>
+<li>The sending of the twelve</li>
+<li>The death of John described</li>
+<li>Herod thinks Jesus is John, risen</li>
+<li>Return of the twelve and feeding of 5000</li>
+<li>Walking on water</li>
+<li>Jesus' discourse on the bread of life</li>
+<li>Healing at Gennesaret</li>
+<li>What defiles a man</li>
+<li>The Syrophenician woman</li>
+<li>The healing of many</li>
+<li>The feeding of 4000</li>
+<li>Peter's confession at Caesarea Philippi; first prediction of the Passion	</li>
+<li>The conditions of discipleship</li>
+<li>The transfiguration</li>
+<li>The coming of Elijah</li>
+<li>The epileptic boy healed</li>
+<li>The second prediction of the passion</li>
+<li>The temple tax</li>
+<li>The dispute about greatness</li>
+<li>The strange exorcist</li>
+<li>The Samaritan villagers</li>
+<li>The nature of discipleship</li>
+<li>Sending out the 12</li>
+<li>Sending out the 70</li>
+<li>WOes on the cities of Galilee</li>
+<li>End of discourse to disciples</li>
+<li>The return of the 70</li>
+<li>Jesus' thanksgiving to the Father</li>
+<li>Lawyer's question and great commandment</li>
+<li>Parable of the Good Samaritan</li>
+<li>Mary and Martha</li>
+<li>The Lord's Prayer</li>
+<li>The friend at midnight</li>
+<li>God's answering of prayer</li>
+<li>The answer to prayer</li>
+<li>The healing of dumb demoniac</li>
+<li>Accusations against Jesus - house divided</li>
+<li>THe wicked generation seek a sign - Jonah</li>
+<li>Parable of salt and light</li>
+<li>SOund eye</li>
+<li>Discourse against Pharisees</li>
+<li>A discourse on leaven</li>
+<li>Blind man at Bethsaida</li>
+<li>Exhortation to fearless confession</li>
+<li>The parable of rich fool</li>
+<li>Confession</li>
+<li>On Anxiety</li>
+<li>Cares about earthly things</li>
+<li>On fasting on treasures</li>
+<li>Watchfulness and faithfulness</li>
+<li>Parable of the ten maidens</li>
+<li>The servant's wages</li>
+<li>Division in households</li>
+<li>Interpreting present time</li>
+<li>On Murder</li>
+<li>On adultery and lust</li>
+<li>Agreement with one's accuser</li>
+<li>Repentance or destruction</li>
+<li>Healing of the woman</li>
+<li>Parables of the mustard seed</li>
+<li>Exclusion from the kingdom</li>
+<li>The narrow gate</li>
+<li>The departure from Galilee</li>
+<li>The lament over Jerusalem</li>
+<li>Healing a man with dropsy</li>
+<li>Teaching on humility</li>
+<li>Parable of marriage feast</li>
+<li>Condition of discipleship</li>
+<li>Cost of discipleshipt</li>
+<li>Concerning salt</li>
+<li>Lost sheep and lost coin</li>
+<li>The Prodigal Son</li>
+<li>The unjust steward</li>
+<li>On serving two masters</li>
+<li>The hypocrisy of the Pharisees</li>
+<li>On the law</li>
+<li>About the law and divorce</li>
+<li>The rich man and lazarus</li>
+<li>ON temptations</li>
+<li>On causing sin</li>
+<li>On reproving one's brother</li>
+<li>On reconciliation</li>
+<li>Parable of the unmerciful servant</li>
+<li>Jesus goes to Jerusalem at the feast of Tabernacles</li>
+<li>Woman taken in adultery brought to Jesus</li>
+<li>Jesus declares Himself light of the world</li>
+<li>Jesus heals blind beggar</li>
+<li>The Good Shepherd</li>
+<li>On forgiveness</li>
+<li>On faith</li>
+<li>The servant's wages</li>
+<li>The healing of ten lepers</li>
+<li>On the kingdom of God</li>
+<li>The culmination of the troubles</li>
+<li>The day of the Son of Man</li>
+<li>The need for watchfulness</li>
+<li>The parable of the unjust judge</li>
+<li>The parable of the Pharisee and the publican</li>
+<li>Marriage and divorce</li>
+<li>Jesus blesses the children</li>
+<li>The rich young man</li>
+<li>The laborers in the vineyard - parable</li>
+<li>Third prediction of the Passion</li>
+<li>The healing of Bartimeus</li>
+<li>Zaccheus</li>
+<li>Parable of the pounds</li>
+<li>Parable of the talents</li>
+<li>The last judgement</li>
+<li>Entry into Jerusalem, Predict destruction of Jerusalem, Jesus in temple</li>
+<li>Jesus keeps feast of Dedication at Jerusalem</li>
+<li>Jesus goes to Bethany, raises Lazarus from the dead.</li>
+<li>Meeting of chief priests and Pharisees</li>
+<li>Jesus at Ephraim</li>
+<li>Cursing of fig tree</li>
+<li>Cleansing of temple</li>
+<li>Meaning of withered fig tree</li>
+<li>Question about authority</li>
+<li>Parable of two sons</li>
+<li>Parable of the wicked tenants</li>
+<li>Question on tribute to Caesar</li>
+<li>Question on the resurrection</li>
+<li>About David's son</li>
+<li>WOes against the Pharisees</li>
+<li>The widow's gift</li>
+<li>Prediction to the destruction of the temple</li>
+<li>The signs of the parousia</li>
+<li>The fate of the disciples</li>
+<li>The beginning of the troubles</li>
+<li>The desolating sacrilege</li>
+<li>The parousia of teh Son of Man</li>
+<li>The parable of the fig tree</li>
+<li>The conspiracy against Jesus</li>
+<li>The traitor</li>
+<li>Lord's Supper</li>
+<li>Last words: the betrayal foretold; greatness in the kingdom of God; Peter's denial prophesied; the two swords</li>
+<li>Jesus prayer and intercessory prayer</li>
+<li>Jesus and the sons of Zebedee</li>
+<li>The betrayal by Judas</li>
+<li>The way to Gethsamane, Peter's betrayal prophesied</li>
+<li>Jesus in Gethsemane</li>
+<li>Jesus taken captive</li>
+<li>Jesus before the Sanhedrin and others; Peter's denial</li>
+<li>Prepartion for the Passover</li>???
+<li>Jesus washes his disciples' feet</li>
+<li>Jesus delivered to pilate</li>
+<li>The death of Judas</li>
+<li>The trial with Pilate</li>
+<li>Jesus is with Herod</li>
+<li>The sentence of death</li>
+<li>The mocking by the soldiers</li>
+<li>The Crucifixion</li>
+<li>The death on the cross; the burial of Jesus</li>
+<li>The guard at the tomb</li>
+<li>The empty tomb</li>
+<li>Bribing the Roman soldiers</li>
+<li>Jesus appears to 2 men on way to Emmaeus</li>
+<li>Jesus appears in Jerusalem</li>
+<li>Jesus appears to the disciples</li>
+<li>Jesus appears at the sea of Tiberius</li>
+<li>Jesus on the mountain in Galilee</li>
+<li>Signs to accompany believers</li>
+<li>The Ascension</li>
+<li>The disciples go out into the world</li>
+<li>John's conclusion to the Gospel</li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
